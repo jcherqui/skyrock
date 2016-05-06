01102ba6fad8db6d2e74509e7f14f321ec844b42
jcherqui <joachim.cherqui@viacesi.fr>
2016-05-06 13:28:37 +0200

    Add Class Radio

--- index.js
+++ index.js
@@ -1,26 +1,65 @@
 const icy = require('icy');
 const lame = require('lame');
 const Speaker = require('speaker');
+const co = require('co');
 
 const skyrock = 'http://firewall.pulsradio.com';
-const metal = 'http://stream.radiometal.com:8010';
+const radiometal = 'http://stream.radiometal.com:8010';
 
-const listen = (radio) => {
-    icy.get(radio, (res) => {
-        res.on('metadata', (metadata) => {
-            const parsed = icy.parse(metadata);
-            console.log(parsed.StreamTitle);
-
-            // DOESN'T MATCH DIFOOL
-            if (radio !== metal && !parsed.StreamTitle.match(/Difool/)) {
-                console.log('change radio');
-                res.unpipe();
-                listen(metal); // METAL
-            }
+const getTitle = (radio) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        icy.get(radio, (res) => {
+            res.on('metadata', (metadata) => {
+                const parsed = icy.parse(metadata);
+                if (!parsed.StreamTitle) {
+                    reject('Error');
+                }
+                resolve(parsed.StreamTitle);
+            });
         });
+    }, 1000);
+});
 
-        res.pipe(new lame.Decoder()).pipe(new Speaker());
-    });
+class Radio {
+    constructor(channel) {
+        this.channel = channel;
+    }
+
+    listen() {
+        this.reader = new Promise((resolve) => {
+            icy.get(this.channel, (res) => {
+                res.pipe(new lame.Decoder()).pipe(new Speaker());
+                resolve(res);
+            });
+        });
+        return this.reader;
+    }
+
+    close() {
+        this.reader.then((reader) => {
+            this.state = 'close';
+            reader.unpipe();
+        });
+    }
+}
+
+const run = function*run() {
+    const radioSkyrock = new Radio(skyrock);
+    const radioMetal = new Radio(radiometal);
+
+    yield radioSkyrock.listen();
+
+    while (true) {
+        const titleRadioSkyrock = yield getTitle(skyrock);
+        console.log(titleRadioSkyrock);
+
+        // IF LISTEN RADIO SKYROCK && TITLE DOESN'T MATCH RADIO LIBRE OF DIFOOL
+        if (radioSkyrock.state !== 'close' && !titleRadioSkyrock.match(/Difool/)) {
+            console.log('change radio');
+            radioSkyrock.close();
+            yield radioMetal.listen();
+        }
+    }
 };
 
-listen(skyrock);
+co(run());
