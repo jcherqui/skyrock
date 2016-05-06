6ab1493368628db17d31ddba0b0af284447b5c2e
ston3o <5039608+ston3o@users.noreply.github.com>
2016-05-06 16:09:14 +0200

    Add File Radio

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
-const icy = require('icy');
-const lame = require('lame');
-const Speaker = require('speaker');
-const co = require('co');
+import co from 'co';
+import Radio from './radio';
 
 const skyrock = 'http://firewall.pulsradio.com';
 const radiometal = 'http://stream.radiometal.com:8010';
 const emission = new RegExp('Difool');
-
-class Radio {
-    constructor(channel) {
-        this.channel = channel;
-    }
-
-    title() {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                icy.get(this.channel, (res) => {
-                    res.on('metadata', (metadata) => {
-                        const parsed = icy.parse(metadata);
-                        resolve(parsed.StreamTitle);
-                    });
-                });
-            }, 1000);
-        });
-    }
-
-    listen() {
-        this.reader = new Promise((resolve) => {
-            icy.get(this.channel, (res) => {
-                res.pipe(new lame.Decoder()).pipe(new Speaker());
-                this.state = 'open';
-                resolve(res);
-            });
-        });
-        return this.reader;
-    }
-
-    close() {
-        this.reader.then((reader) => {
-            this.state = 'close';
-            reader.unpipe();
-        });
-    }
-}
 
 const run = function*run() {
     const radioSkyrock = new Radio(skyrock);
